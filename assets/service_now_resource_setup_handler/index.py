7891c27d37c81117364a4a5a9976c705d132387b
Olalekan Fagbo <81133627+nuel-solutions@users.noreply.github.com>
2025-10-20 20:39:36 -0500

    Update index.py

--- assets/service_now_resource_setup_handler/index.py
+++ assets/service_now_resource_setup_handler/index.py
@@ -505,7 +505,7 @@ class ServiceNowApiService:
             return None
 
 
-def lambda_handler(event, context):
+def handler(event, context):
     """
     Custom resource handler to create ServiceNow resources.
 
